5fce11ce4ad7a47602f36667109fd6e0e78a976c
HarlockOfficial <harlockofficial.github@gmail.com>
2021-01-26 23:16:21 +0100

    Bellman Ford algorithm & JUnit test

--- src/it/unicam/cs/asdl2021/totalproject2/BellmanFordShortestPathComputer.java
+++ src/it/unicam/cs/asdl2021/totalproject2/BellmanFordShortestPathComputer.java
@@ -1,5 +1,7 @@
 package it.unicam.cs.asdl2021.totalproject2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,8 +16,9 @@ import java.util.List;
  */
 public class BellmanFordShortestPathComputer<L>
         implements SingleSourceShortestPathComputer<L> {
-
-    // TODO inserire le variabili istanza necessarie
+    private final Graph<L> graph;
+    private boolean solved;
+    private GraphNode<L> lastSource;
 
     /**
      * Crea un calcolatore di cammini minimi a sorgente singola per un grafo
@@ -38,39 +41,115 @@ public class BellmanFordShortestPathComputer<L>
      *                                      peso è {@code Double.NaN}.
      */
     public BellmanFordShortestPathComputer(Graph<L> graph) {
-        // TODO implementare
+        if(graph==null){
+            throw new NullPointerException("Passed parameter must be not null");
+        }
+        if(graph.isEmpty() || !graph.isDirected() || graph.getEdges().isEmpty()){
+            throw new IllegalArgumentException("Invalid Graph passed");
+        }
+        for(GraphEdge<L> edge: graph.getEdges()){
+            if(!edge.hasWeight()){
+                throw new IllegalArgumentException("Invalid Graph passed");
+            }
+        }
+        this.graph=graph;
+        solved=false;
+        lastSource=null;
     }
 
     @Override
     public void computeShortestPathsFrom(GraphNode<L> sourceNode) {
-        // TODO implementare
+        if(sourceNode==null){
+            throw new NullPointerException("Passed parameter must be not null");
+        }
+        if(!graph.getNodes().contains(sourceNode)){
+            throw new IllegalArgumentException("Invalid Node passed");
+        }
+        // I decided to use only the floating point distance
+        for(GraphNode<L> node: graph.getNodes()){
+            node.setFloatingPointDistance(Double.POSITIVE_INFINITY);
+            node.setPrevious(null);
+            if(sourceNode.equals(node)){
+                node.setFloatingPointDistance(0.0);
+            }
+        }
+        for(int i=0;i<graph.getNodes().size();++i){
+            for(GraphEdge<L> edge:graph.getEdges()){
+                GraphNode<L> source = edge.getNode1();
+                GraphNode<L> dest = edge.getNode2();
+                if(dest.getFloatingPointDistance()>source.getFloatingPointDistance()+edge.getWeight()){
+                    dest.setFloatingPointDistance(source.getFloatingPointDistance()+edge.getWeight());
+                    dest.setPrevious(source);
+                }
+            }
+        }
+        for(GraphEdge<L> edge: graph.getEdges()){
+            GraphNode<L> source = edge.getNode1();
+            GraphNode<L> dest = edge.getNode2();
+            if(dest.getFloatingPointDistance()>source.getFloatingPointDistance()+edge.getWeight()){
+                throw new IllegalStateException("Graph contains negative-weight cycle");
+            }
+        }
 
+        lastSource=sourceNode;
+        solved=true;
     }
 
     @Override
     public boolean isComputed() {
-        // TODO implementare
-        return false;
+        return solved;
     }
 
     @Override
     public GraphNode<L> getLastSource() {
-        // TODO implementare
-        return null;
+        if(lastSource==null){
+            throw new IllegalStateException("Must solve at least once before calling this function");
+        }
+        return lastSource;
     }
 
     @Override
     public Graph<L> getGraph() {
-        // TODO implementare
-        return null;
+        return graph;
     }
 
+    //TODO check might be wrong
     @Override
     public List<GraphEdge<L>> getShortestPathTo(GraphNode<L> targetNode) {
-        // TODO implementare
-        return null;
+        if(targetNode==null){
+            throw new NullPointerException("Passed parameter must be not null");
+        }
+        if(!graph.getNodes().contains(targetNode)){
+            throw new IllegalArgumentException("Invalid Node passed");
+        }
+        if(lastSource==null){
+            throw new IllegalStateException("Must solve at least once before calling this function");
+        }
+        List<GraphEdge<L>> shortestPath = new ArrayList<>();
+        GraphNode<L> lastNode = null;
+        for(GraphNode<L> node: graph.getNodes()){
+            if(targetNode.equals(node)){
+                lastNode = node;
+                break;
+            }
+        }
+        //unnecessary assert, checked in if in line 122, done to avoid warning in line 139
+        assert(lastNode!=null);
+        while (true){
+            GraphNode<L> prev = lastNode.getPrevious();
+            for(GraphEdge<L> edge: graph.getIngoingEdgesOf(lastNode)){
+                if(edge.getNode1().equals(prev)) {
+                    shortestPath.add(edge);
+                    break;
+                }
+            }
+            if(prev.equals(lastSource)){
+                break;
+            }
+            lastNode=prev;
+        }
+
+        Collections.reverse(shortestPath);
+        return shortestPath;
     }
-
-    // TODO inserire eventuali altri metodi privati per scopi di implementazione
-
 }
--- src/it/unicam/cs/asdl2021/totalproject2/BellmanFordShortestPathComputerTest.java
+++ src/it/unicam/cs/asdl2021/totalproject2/BellmanFordShortestPathComputerTest.java
@@ -4,43 +4,130 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * 
  * @author Template: Luca Tesei
  *
  */
 class BellmanFordShortestPathComputerTest {
-
-    // TODO implementare tutti i test non ancora implementati
-
     @Test
     final void testBellmanFordShortestPathComputer() {
-        fail("Not yet implemented"); // TODO
+        //noinspection ConstantConditions
+        assertThrows(NullPointerException.class, () -> new BellmanFordShortestPathComputer<Integer>(null));
+
+        Graph<Integer> undirectedGraph = new MapAdjacentListUndirectedGraph<>();
+        GraphNode<Integer> undirectedGraphNode = new GraphNode<>(1);
+        undirectedGraph.addNode(undirectedGraphNode);
+        undirectedGraph.addEdge(new GraphEdge<>(undirectedGraphNode, undirectedGraphNode, false, 12));
+        assertThrows(IllegalArgumentException.class, () -> new BellmanFordShortestPathComputer<>(undirectedGraph));
+
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        assertThrows(IllegalArgumentException.class, () -> new BellmanFordShortestPathComputer<>(graph));
+        GraphNode<Integer> node = new GraphNode<>(1);
+        graph.addNode(node);
+        assertThrows(IllegalArgumentException.class, () -> new BellmanFordShortestPathComputer<>(graph));
+        graph.addEdge(new GraphEdge<>(node, node, true));
+        assertThrows(IllegalArgumentException.class, () -> new BellmanFordShortestPathComputer<>(graph));
+        graph.removeEdge(new GraphEdge<>(node, node, true));
+        graph.addEdge(new GraphEdge<>(node, node, true, 12));
+        assertEquals(graph, new BellmanFordShortestPathComputer<>(graph).getGraph());
     }
 
     @Test
     final void testComputeShortestPathsFrom() {
-        fail("Not yet implemented"); // TODO
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        GraphNode<Integer> node1 = new GraphNode<>(1);
+        GraphNode<Integer> node2 = new GraphNode<>(2);
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addEdge(new GraphEdge<>(node1, node2, true, 9));
+        SingleSourceShortestPathComputer<Integer> computer = new BellmanFordShortestPathComputer<>(graph);
+        assertThrows(NullPointerException.class, () -> computer.computeShortestPathsFrom(null));
+        assertThrows(IllegalArgumentException.class, () -> computer.computeShortestPathsFrom(new GraphNode<>(3)));
+        GraphEdge<Integer> edge = new GraphEdge<>(node2, node1, true, -10);
+        graph.addEdge(edge);
+        assertThrows(IllegalStateException.class, () -> computer.computeShortestPathsFrom(node1));
+        graph.removeEdge(edge);
+        edge = new GraphEdge<>(node2, node1, true, 2);
+        graph.addEdge(edge);
+        assertFalse(computer.isComputed());
+        computer.computeShortestPathsFrom(node1);
+        assertTrue(computer.isComputed());
     }
 
     @Test
     final void testIsComputed() {
-        fail("Not yet implemented"); // TODO
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        GraphNode<Integer> node1 = new GraphNode<>(1);
+        GraphNode<Integer> node2 = new GraphNode<>(2);
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addEdge(new GraphEdge<>(node1, node2, true, 9));
+        graph.addEdge(new GraphEdge<>(node2, node1, true, 2));
+        SingleSourceShortestPathComputer<Integer> computer = new BellmanFordShortestPathComputer<>(graph);
+        assertFalse(computer.isComputed());
+        computer.computeShortestPathsFrom(node1);
+        assertTrue(computer.isComputed());
     }
 
     @Test
     final void testGetLastSource() {
-        fail("Not yet implemented"); // TODO
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        GraphNode<Integer> node1 = new GraphNode<>(1);
+        GraphNode<Integer> node2 = new GraphNode<>(2);
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addEdge(new GraphEdge<>(node1, node2, true, 9));
+        graph.addEdge(new GraphEdge<>(node2, node1, true, 2));
+        SingleSourceShortestPathComputer<Integer> computer = new BellmanFordShortestPathComputer<>(graph);
+        assertFalse(computer.isComputed());
+        //noinspection Convert2MethodRef
+        assertThrows(IllegalStateException.class, () -> computer.getLastSource());
+        computer.computeShortestPathsFrom(node2);
+        assertTrue(computer.isComputed());
+        assertEquals(node2, computer.getLastSource());
     }
 
     @Test
     final void testGetGraph() {
-        fail("Not yet implemented"); // TODO
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        Random generator = new Random(123456);
+        for(int i=0;i<100;++i){
+            graph.addNode(new GraphNode<>(generator.nextInt()));
+        }
+        for(int i=0;i<4000;++i){
+            GraphNode<Integer> node1 = graph.getNodeAtIndex(generator.nextInt(graph.nodeCount()));
+            GraphNode<Integer> node2 = graph.getNodeAtIndex(generator.nextInt(graph.nodeCount()));
+            graph.addEdge(new GraphEdge<>(node1, node2, true, generator.nextInt()));
+        }
+        //assuming that constructor works
+        SingleSourceShortestPathComputer<Integer> computer = new BellmanFordShortestPathComputer<>(graph);
+        assertEquals(computer.getGraph(), graph);
     }
 
     @Test
     final void testGetShortestPathTo() {
-        fail("Not yet implemented"); // TODO
+        Graph<Integer> graph = new AdjacencyMatrixDirectedGraph<>();
+        GraphNode<Integer> node1 = new GraphNode<>(1);
+        GraphNode<Integer> node2 = new GraphNode<>(2);
+        graph.addNode(node1);
+        graph.addNode(node2);
+        GraphEdge<Integer> edge1 = new GraphEdge<>(node1, node2, true, 9);
+        graph.addEdge(edge1);
+        graph.addEdge(new GraphEdge<>(node2, node1, true, 2));
+        SingleSourceShortestPathComputer<Integer> computer = new BellmanFordShortestPathComputer<>(graph);
+        assertThrows(IllegalStateException.class, () -> computer.getShortestPathTo(node1));
+        computer.computeShortestPathsFrom(node1);
+        assertThrows(NullPointerException.class, () -> computer.getShortestPathTo(null));
+        assertThrows(IllegalArgumentException.class, () -> computer.getShortestPathTo(new GraphNode<>(3)));
+        List<GraphEdge<Integer>> shortestPath = computer.getShortestPathTo(node2);
+        List<GraphEdge<Integer>> edges = new ArrayList<>();
+        edges.add(edge1);
+        assertEquals(edges, shortestPath);
     }
 
 }
